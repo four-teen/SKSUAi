8212e73cd5bc693e4464c16243e3d680f66ef76e
Elbren Antonio <elbrenantonio@sksu.edu.ph>
2023-02-24 12:45:49 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server - whahwhwh dito banda
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://ewa-36sf.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'

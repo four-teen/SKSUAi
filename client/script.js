38e38155bd20fdc617bd4a4ee78b891b6d535b79
Elbren Antonio <elbrenantonio@sksu.edu.ph>
2023-02-25 09:04:52 +0800

    Add .gitignore file

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server - whahwhwh dito banda
 
-  const response = await fetch('https://ewa-36sf.onrender.com', {
+  const response = await fetch('http://localhost:5000/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
